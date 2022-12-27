ae1d636251a1ca22f1018a5568ac9d9bc2c6eec6
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-12-27 12:21:35 -0300

    Prox. Aula -> Configurando Stitches

--- autogit.sh
+++ autogit.sh
@@ -1,5 +1,5 @@
 git add .
-git commit -m "Prox. Aula -> Configurando documento HTML"
+git commit -m "Prox. Aula -> Configurando Stitches"
 git branch -M main
 git remote add origin https://github.com/CarlosViniMSouza/ignite-shop.git
 git push -u origin main
